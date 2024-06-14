66f4757d6081dd9ad167937a33b25962ce3885b8
Marian Kočenda <marian.kocenda@leris.cz>
2024-06-14 13:24:54 +0200

    Templates + Basepresenter

---
+++ app/app/Presenters/BasePresenter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\App\Presenter;
+
+class BasePresenter extends \App\Presenter\BasePresenter
+{
+
+	public function startup()
+	{
+		parent::startup();
+	}
+
+	public function beforeRender()
+	{
+		parent::beforeRender();
+	}
+
+}
