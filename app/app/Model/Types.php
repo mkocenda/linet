d8998142237254fd4901029a2a5bc732de920851
Marian Kočenda <marian.kocenda@leris.cz>
2024-06-14 13:25:16 +0200

    Types"model"

---
+++ app/app/Model/Types.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\App\Model;
+
+class Types
+{
+    const STATUS = array('NEW' => 'new', 'ACT' => 'active', 'END' => 'closed');
+    const contractType = array('partner-sale' => 'partner', 'customer-sale' => 'customer');
+    
+    
+}
